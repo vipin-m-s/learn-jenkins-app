9ec03254f998c133eea9fb6df9adc6190230717e
Vipin M S <vipinachar2016@gmail.com>
2026-07-16 07:28:51 +0530

    etst

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         </a>
       </header>
       <p>
-          Application version: {REACT_APP_VERSION}
+          Application version: {process.env.REACT_APP_VERSION}
       </p>
     </div>
   );
